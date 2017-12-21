0cdce5c925f58622d3bf56cf6e5a7a0bb184cd78
UDICatNCHU <yfan@nchu.edu.tw>
2017-12-21 10:01:01 +0800

    update 20171221 簡化wordcount程式碼

--- src/WordCount.java
+++ src/WordCount.java
@@ -13,15 +13,12 @@ import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 public class WordCount {
         
  public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
-    private final static IntWritable one = new IntWritable(1);
-    private Text word = new Text();
-        
     public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         String line = value.toString();
         StringTokenizer tokenizer = new StringTokenizer(line);
         while (tokenizer.hasMoreTokens()) {
-            word.set(tokenizer.nextToken());
-            context.write(word, one);
+            String token = tokenizer.nextToken();
+            context.write(new Text(token), new IntWritable(1));
         }
     }
  } 
