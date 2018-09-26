4365720f3db4a516838dc8268c2a5b7d3ecac691
UDICatNCHU <yfan@drogran.nchu.edu.tw>
2018-09-26 06:48:35 +0000

    add sort example

--- src/Sort.java
+++ src/Sort.java
@@ -10,38 +10,34 @@ import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
         
-public class Sum2 {
+public class Sort {
         
- public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
+ public static class Map extends Mapper<LongWritable, Text, IntWritable, Text> {
     public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         String line = value.toString();
         StringTokenizer tokenizer = new StringTokenizer(line);
         while (tokenizer.hasMoreTokens()) {
             String token = tokenizer.nextToken();
-            context.write(new Text("what ever you like"), new IntWritable(Integer.parseInt(token)));
+            context.write(new IntWritable(Integer.parseInt(token)), new Text());
         }
     }
  } 
         
- public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
+ public static class Reduce extends Reducer<IntWritable, Text, IntWritable, Text> {
 
-    public void reduce(Text key, Iterable<IntWritable> values, Context context) 
+    public void reduce(IntWritable key, Iterable<Text> values, Context context) 
       throws IOException, InterruptedException {
-        int sum = 0;
-        for (IntWritable val : values) {
-            sum += val.get();
-        }
-        context.write(key, new IntWritable(sum));
+        context.write(key, new Text());
     }
  }
         
  public static void main(String[] args) throws Exception {
     Configuration conf = new Configuration();
         
-        Job job = new Job(conf, "wordcount");
+    Job job = new Job(conf, "sort");
     
-    job.setOutputKeyClass(Text.class);
-    job.setOutputValueClass(IntWritable.class);
+    job.setOutputKeyClass(IntWritable.class);
+    job.setOutputValueClass(Text.class);
         
     job.setMapperClass(Map.class);
     job.setReducerClass(Reduce.class);
