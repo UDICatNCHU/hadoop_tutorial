130fb62529eaf4ab70579f9e469d909d0e079401
yfan <yfan@nchu.edu.tw>
2020-10-06 12:14:30 +0000

    update trueSort

---
+++ src/TrueSort.java
@@ -0,0 +1,57 @@
+import java.io.IOException;
+import java.util.*;
+        
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+        
+public class TrueSort {
+        
+ public static class Map extends Mapper<LongWritable, Text, IntWritable, Text> {
+    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        String line = value.toString();
+        StringTokenizer tokenizer = new StringTokenizer(line);
+        while (tokenizer.hasMoreTokens()) {
+            String token = tokenizer.nextToken();
+            context.write(new IntWritable(Integer.parseInt(token)), new Text());
+        }
+    }
+ } 
+        
+ public static class Reduce extends Reducer<IntWritable, Text, IntWritable, Text> {
+
+    public void reduce(IntWritable key, Iterable<Text> values, Context context) 
+      throws IOException, InterruptedException {
+        for (Text value: values){
+	context.write(key, new Text());
+        }
+    }
+ }
+        
+ public static void main(String[] args) throws Exception {
+    Configuration conf = new Configuration();
+        
+    Job job = new Job(conf, "sort");
+    
+    job.setOutputKeyClass(IntWritable.class);
+    job.setOutputValueClass(Text.class);
+        
+    job.setMapperClass(Map.class);
+    job.setReducerClass(Reduce.class);
+    job.setJarByClass(WordCount.class);
+        
+    job.setInputFormatClass(TextInputFormat.class);
+    job.setOutputFormatClass(TextOutputFormat.class);
+        
+    FileInputFormat.addInputPath(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        
+    job.waitForCompletion(true);
+ }
+        
+}
