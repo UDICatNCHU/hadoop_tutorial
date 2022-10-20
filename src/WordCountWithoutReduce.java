a1e9a2979bd2cb6b92813a9921afde3273e7f667
yaochung <yfan@nchu.edu.tw>
2022-10-20 14:48:43 +0800

    add homework

--- src/WordCountWithoutReduce.java
+++ src/WordCountWithoutReduce.java
@@ -10,18 +10,15 @@ import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
         
-public class SortWordCount {
+public class WordCountWithoutReduce {
         
  public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
-    private final static IntWritable one = new IntWritable(1);
-    private Text word = new Text();
-        
     public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         String line = value.toString();
         StringTokenizer tokenizer = new StringTokenizer(line);
         while (tokenizer.hasMoreTokens()) {
-            word.set(tokenizer.nextToken());
-            context.write(word, one);
+            String token = tokenizer.nextToken();
+            context.write(new Text(token), new IntWritable(1));
         }
     }
  } 
@@ -38,64 +35,25 @@ public class SortWordCount {
     }
  }
         
-
-
- public static class SortMap extends Mapper<LongWritable, Text, IntWritable, Text> {
-   
-
-    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        String line = value.toString();
-        StringTokenizer tokenizer = new StringTokenizer(line);
-            String token1 = tokenizer.nextToken();
-            String token2 = tokenizer.nextToken();
-            context.write(new IntWritable(Integer.parseInt(token2)), new Text(token1));
-        
-    }
- }
-
- public static class SortReduce extends Reducer<IntWritable, Text, IntWritable, Text> {
-
-    public void reduce(IntWritable key, Iterable<Text> values, Context context)
-      throws IOException, InterruptedException {
-        for (Text val : values) {
-            context.write(key, val);
-        }
-    }
- }
-
-
-
-
-
-
  public static void main(String[] args) throws Exception {
     Configuration conf = new Configuration();
         
-    Job job = new Job(conf, "wordcount");   
+    Job job = new Job(conf, "wordcount");
+    
     job.setOutputKeyClass(Text.class);
     job.setOutputValueClass(IntWritable.class);
+        
     job.setMapperClass(Map.class);
-    job.setReducerClass(Reduce.class);
+    // job.setReducerClass(Reduce.class);
     job.setJarByClass(WordCount.class);
+        
     job.setInputFormatClass(TextInputFormat.class);
     job.setOutputFormatClass(TextOutputFormat.class);
+        
     FileInputFormat.addInputPath(job, new Path(args[0]));
     FileOutputFormat.setOutputPath(job, new Path(args[1]));
         
     job.waitForCompletion(true);
-    Configuration conf2 = new Configuration();
-    Job job2 = new Job(conf2, "sort_count");
-    job2.setOutputKeyClass(IntWritable.class);
-    job2.setOutputValueClass(Text.class);
-    job2.setMapperClass(SortMap.class);
-    job2.setReducerClass(SortReduce.class);
-    job2.setJarByClass(WordCount.class);
-    job2.setInputFormatClass(TextInputFormat.class);
-    job2.setOutputFormatClass(TextOutputFormat.class);
-    FileInputFormat.addInputPath(job2, new Path(args[1]));
-    FileOutputFormat.setOutputPath(job2, new Path(args[2]));
-    job2.waitForCompletion(true);
-
  }
         
 }
